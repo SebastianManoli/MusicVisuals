b3bfc2c0a5bfa494ac43234e2454e8adc3b3ab7f
SebastianManoli <C22375341@mytudublin.ie>
2024-04-19 23:10:20 +0100

    got the spotlights moving on their own pretty cool

--- java/src/c123456/SebsVisual.java
+++ java/src/c123456/SebsVisual.java
@@ -8,6 +8,11 @@ public class SebsVisual extends Visual
 
     MainVisual main;
 
+    float angleX = 200;
+    float angleY = 0;
+    float speedX = 0.05f;
+    float speedY = 0.05f;
+
     public SebsVisual(MainVisual main) {
         this.main = main;
     }
@@ -19,11 +24,19 @@ public class SebsVisual extends Visual
         main.background(0);
         main.noStroke();
 
-        
-        main.directionalLight(51, 102, 126, 0, -1, 0);
+        main.directionalLight(225, 102, 126, 0, -1, 0);
+        main.directionalLight(100, 102, 126, 0, 1, 0);
 
-        main.spotLight(130, 300, 204, main.mouseX, 360, 600, 0, 0, -1, MainVisual.PI/2, 600);
-        main.spotLight(130, 300, 204, 360, main.mouseY, 600, 0, 0, -1, MainVisual.PI/2, 600);
+        // Calculate spotlight positions
+        float spotlightX = main.width / 2 + 90 * PApplet.sin(angleX);
+        float spotlightY = main.height / 2 + 90 * PApplet.sin(angleY);
+
+        main.spotLight(130, 300, 204, spotlightX, 360, 600, 0, 0, -1, MainVisual.PI/2, 600);
+        main.spotLight(130, 300, 204, 360, spotlightY, 600, 0, 0, -1, MainVisual.PI/2, 600);
+
+        // Update angles for animation
+        angleX += speedX;
+        angleY += speedY;
 
         main.translate(main.width/2, main.height/2, 0);
 
