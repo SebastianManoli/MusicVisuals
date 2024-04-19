9c93a761225a3fd9b3b8d24c57dd8871be43e01b
Danelle Pillay <C22348731@mytudublin.ie>
2024-04-19 16:37:53 +0100

    form base code of DanellesVisual

--- java/src/c123456/DanellesVisual.java
+++ java/src/c123456/DanellesVisual.java
@@ -1,5 +1,21 @@
 package c123456;
 
-public class DanellesVisual {
+import ie.tudublin.*;
+import processing.core.PApplet;
+
+public class DanellesVisual extends Visual{
+    MainVisual main;
+
+    public DanellesVisual(MainVisual main){
+        this.main = main;
+    }
+
+    public void render(){
+        main.colorMode(MainVisual.HSB);
+        main.textSize(20);
+        main.fill(255);
+        main.textAlign(CENTER,CENTER);
+
+    }
     
 }
