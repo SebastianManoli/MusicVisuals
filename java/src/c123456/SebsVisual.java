a4c61fe290e30bc7ab01fe275d61b3877d049654
SebastianManoli <C22375341@mytudublin.ie>
2024-04-19 22:41:32 +0100

    Initial visual idea of a sphere with orbiting spotlights

--- java/src/c123456/SebsVisual.java
+++ java/src/c123456/SebsVisual.java
@@ -3,7 +3,8 @@ package c123456;
 import ie.tudublin.*;
 import processing.core.PApplet;
 
-public class SebsVisual extends Visual {
+public class SebsVisual extends Visual
+{
 
     MainVisual main;
 
@@ -11,11 +12,22 @@ public class SebsVisual extends Visual {
         this.main = main;
     }
 
-    public void render() 
+    public void render()
     {
         main.colorMode(MainVisual.HSB);
+        main.sphereDetail(150);
         main.background(0);
+        main.noStroke();
+
         
+        main.directionalLight(51, 102, 126, 0, -1, 0);
+
+        main.spotLight(130, 300, 204, main.mouseX, 360, 600, 0, 0, -1, MainVisual.PI/2, 600);
+        main.spotLight(130, 300, 204, 360, main.mouseY, 600, 0, 0, -1, MainVisual.PI/2, 600);
+
+        main.translate(main.width/2, main.height/2, 0);
+
+        main.sphere(120);
     }
 
 }
