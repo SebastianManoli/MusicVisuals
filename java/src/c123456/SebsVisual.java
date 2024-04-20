fde66855ef8f87e69708e997958e3092961334eb
SebastianManoli <C22375341@mytudublin.ie>
2024-04-20 18:16:50 +0100

    Got the sphere size to respond to the music, also the spotlights to move randomly within the sphere but theyre moving too fast at the moment.

--- java/src/c123456/SebsVisual.java
+++ java/src/c123456/SebsVisual.java
@@ -8,10 +8,13 @@ public class SebsVisual extends Visual
 
     MainVisual main;
 
+    float angle = 0;
     float angleX = 200;
     float angleY = 0;
-    float speedX = 0.05f;
-    float speedY = 0.05f;
+    float speedX = 0.01f;
+    float speedY = 0.01f;
+    int numBands = 8; // Number of frequency bands
+    float bandWidth; // Width of each band
 
     public SebsVisual(MainVisual main) {
         this.main = main;
@@ -24,23 +27,48 @@ public class SebsVisual extends Visual
         main.background(0);
         main.noStroke();
 
+        main.calculateAverageAmplitude();
+
+
         main.directionalLight(225, 102, 126, 0, -1, 0);
         main.directionalLight(100, 102, 126, 0, 1, 0);
 
-        // Calculate spotlight positions
-        float spotlightX = main.width / 2 + 90 * PApplet.sin(angleX);
-        float spotlightY = main.height / 2 + 90 * PApplet.sin(angleY);
+        // Generate random angles for spotlight positions
+        float randomAngleX = main.random(MainVisual.TWO_PI);
+        float randomAngleY = main.random(MainVisual.TWO_PI);
 
-        main.spotLight(130, 300, 204, spotlightX, 360, 600, 0, 0, -1, MainVisual.PI/2, 600);
-        main.spotLight(130, 300, 204, 360, spotlightY, 600, 0, 0, -1, MainVisual.PI/2, 600);
+        // Calculate spotlight positions
+        float spotlightX = main.width / 2 + 90 * PApplet.sin(randomAngleX);
+        float spotlightY = main.height / 2 + 90 * PApplet.sin(randomAngleY);
 
         // Update angles for animation
-        angleX += speedX;
-        angleY += speedY;
+        randomAngleX += speedX;
+        randomAngleY += speedY;
+
+        main.spotLight(130, 300, 204, spotlightX, spotlightY, 600, 0, 0, -1, MainVisual.PI/2, 600);
+        main.spotLight(20, 300, 204, spotlightY, spotlightY, 600, 0, 0, -1, MainVisual.PI/2, 600);
+
 
         main.translate(main.width/2, main.height/2, 0);
 
-        main.sphere(120);
+        float sphereSize = 100 + (300 * main.getSmoothedAmplitude()); 
+        main.sphere(sphereSize);
+
+        // main.calculateAverageAmplitude();
+        // main.stroke(MainVisual.map(main.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        // main.strokeWeight(5);
+        // main.noFill();
+        // main.lights();
+        // main.pushMatrix();
+        // //
+        // main.camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
+        // main.translate(0, 0, -200);
+        // main.rotateX(angle);
+        // main.rotateZ(angle);       
+        // float boxSize = 50 + (200 * main.getSmoothedAmplitude()); 
+        // main.box(boxSize);   
+        // main.popMatrix();
+        // angle += 0.01f;
     }
 
 }
