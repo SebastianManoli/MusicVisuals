6e1c81fa4a6f96d4e9ac2bb5a5de9a8fe879a435
Danelle Pillay <C22348731@mytudublin.ie>
2024-04-21 00:48:28 +0100

    completed collide method

--- java/src/c123456/DanellesVisual.java
+++ java/src/c123456/DanellesVisual.java
@@ -13,13 +13,28 @@ public class DanellesVisual extends Visual
     float spring = 0.05f;
     float friction = -0.9f;
 
+    public DanellesVisual(MainVisual main){
+        this.main = main;
+    }
 
+    public void render(){
+        for (int i = 0; i < numBalls; i++)
+        {
+            balls[i] = new Ball(main.random(main.width), main.random(main.height), main.random(30, 70), i, balls);
+        
+        }
 
+        main.noStroke();
+        main.fill(255, 204);
 
+        main.background(0);
+        for (Ball ball : balls) {
+            ball.collide();
+            //ball.move();
+            //ball.display(;)
+        }
+    }
 
-
-
-}
 
 //class that holds methods and atttributes for the balls' movement, collision and shape and colour
 class Ball 
@@ -41,6 +56,28 @@ class Ball
         others = oin;
     }
 
+    void collide()
+    {
+        for (int i = id + 1; i < numBalls; i++)
+        {
+            float dx = others[i].x - x;
+            float dy = others[i].y - y;
+            float distance = main.sqrt(dx*dx + dy*dy);
+            float minDist = others[i].diameter/2 + diameter/2;
+
+            //if the balls collide
+            if (distance < minDist)
+            {
+                float angle = main.atan2(dy, dx);
+                float targetX = x + main.cos(angle) * minDist;
+                float targetY = y + main.sin(angle) * minDist;
+                float ax = (targetX - others[i].x) * spring;
+                float ay = (targetY - others[i].y) * spring;
+                vx -= ax;
+                vy -= ay;
+            }
+        }
+    }
 
 
 
@@ -57,6 +94,11 @@ class Ball
 
 
 
+
+
+
+
+}
 
 }
 
