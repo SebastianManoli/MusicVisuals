33a131a65dcd41191d5c8986edc32bb206a99fc0
SebastianManoli <C22375341@mytudublin.ie>
2024-04-23 12:59:44 +0100

    Created a joint visual java file

---
+++ java/src/c123456/JointVisual.java
@@ -0,0 +1,21 @@
+package c123456;
+
+import ie.tudublin.*;
+
+public class JointVisual extends Visual
+{
+
+    MainVisual main;
+
+    
+
+    public JointVisual(MainVisual main) {
+        this.main = main;
+    }
+
+    public void render()
+    {
+        
+    }
+
+}
