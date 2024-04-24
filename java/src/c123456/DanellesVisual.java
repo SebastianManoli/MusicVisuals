0f5687baacef95017de497d9f552ef2a10482714
Danelle Pillay <C22348731@mytudublin.ie>
2024-04-24 14:36:44 +0100

    Added comments to my visual's code where necessary

--- java/src/c123456/DanellesVisual.java
+++ java/src/c123456/DanellesVisual.java
@@ -12,11 +12,12 @@ public class DanellesVisual extends Visual
     float spring = 0.05f;
     float gravity = 0.05f;
     float friction = -0.9f;
-    Ball[] balls = new Ball[numBalls];
-    float smoothedAmplitude;
+    float smoothedAmplitude; 
     float[] lerpedBuffer;
 
-
+    //create an array of balls
+    Ball[] balls = new Ball[numBalls]; 
+    
     public DanellesVisual(MainVisual main){
         this.main = main;
     }
@@ -25,20 +26,28 @@ public class DanellesVisual extends Visual
 
         float average = 0;
         float sum = 0;
+
+        //calculate half the height of the screen
         float halfHeight = main.height / 2;
+
+        //create a new array of floats that is the width of the screen
         lerpedBuffer = new float[main.width];
         smoothedAmplitude = MainVisual.lerp(smoothedAmplitude, average, 0.1f);
 
+        //for loop to iterate through the audio buffer and calculate the average
         for (int i = 0; i < main.getAudioBuffer().size(); i++) 
         {
+            //sum of all the values in the audio buffer
             sum += MainVisual.abs(main.getAudioBuffer().get(i));
             lerpedBuffer[i] = MainVisual.lerp(lerpedBuffer[i], main.getAudioBuffer().get(i), 0.05f);
         }
+
+        //this calculates the average of the sum of the values in the audio buffer
         average = sum / (float) main.getAudioBuffer().size();
 
         sum = 0;
 
-        
+        //set the stroke, stroke weight and fill to change with the amplitude of the song
         main.stroke(MainVisual.map(10*main.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
         main.strokeWeight(2);
         main.fill(MainVisual.map(10*main.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
@@ -47,21 +56,23 @@ public class DanellesVisual extends Visual
         main.colorMode(MainVisual.HSB);
 
         main.background(0);
+
+        //for loop that iterates through the balls array and calls the methods
         for (Ball ball : balls) {
             ball.collide();
             ball.move();
             ball.display();
-            //ball.changeColor();
         }
 
         for(int i =0; i < main.getAudioBuffer().size(); i++)
         {
-            // These are to map through the colour specturm and change colours while visualising
+            // Variables to map through the colour spectrum and change colours while visualising
             float c = Visual.map(i, 0, main.getAudioBuffer().size(), 0, 255);
             float c2 = Visual.map(i, 0, main.getAudioBuffer().size(), 255, 0);
             
-            // These will add the border to all 4 sides and will be changed with the frequency of the song
+            // Adds the border to all 4 sides and will be changed with the frequency of the song
             main.stroke(c, 255, 255);
+
             float side1 = lerpedBuffer[i] * halfHeight * 2.5f;
             main.rect(i*2, halfHeight*1.95f + side1, i, halfHeight - side1);    
             
@@ -103,21 +114,31 @@ public class Ball
     //method so that the balls move and bounce off sides
     void collide()
     {
+        //for loop to iterate through the balls and check if they collide 
         for (int i = id + 1; i < numBalls; i++)
         {
             float dx = others[i].x - x;
             float dy = others[i].y - y;
+
+            //calculate the distance between the balls
             float distance = MainVisual.sqrt(dx*dx + dy*dy);
+
+            //calculate the minimum distance between the balls and set it to change with the amplitude of the song
             float minDist = others[i].diameter/2 + main.getSmoothedAmplitude()*350 + diameter/2 + main.getSmoothedAmplitude()*350;
 
             //if the balls collide
             if (distance < minDist)
             {
+                //calculate the angle and target x and y
                 float angle = MainVisual.atan2(dy, dx);
                 float targetX = x + MainVisual.cos(angle) * minDist;
                 float targetY = y + MainVisual.sin(angle) * minDist;
+
+                //calculate the new velocity
                 float ax = (targetX - others[i].x) * spring;
                 float ay = (targetY - others[i].y) * spring;
+
+                //change the velocity
                 vx -= ax;
                 vy -= ay;
                 others[i].vx += ax;
@@ -126,19 +147,24 @@ public class Ball
         }
     }
 
+    //method that controls the movement of the balls
     void move(){
         vy += gravity;
         x += vx;
         y += vy;
 
+        //if the balls hit the sides of the screen
         if (x + diameter/2 > main.width) {
             x = main.width - diameter/2;
             vx *= friction;
         }
+
         else if (x - diameter/2 < 0) {
             x = diameter/2;
             vx *= friction;
         }
+
+        //if the balls hit the top or bottom of the screen
         if (y + diameter/2 > main.height) {
             y = main.height - diameter/2;
             vy *= friction;
@@ -149,7 +175,9 @@ public class Ball
         }
     }
 
+    //method to display the balls
     void display() {
+        //set the fill to change with the amplitude of the song
         main.ellipse(x, y, diameter+ main.getSmoothedAmplitude()*350, diameter+ main.getSmoothedAmplitude()*350);
     }
 
